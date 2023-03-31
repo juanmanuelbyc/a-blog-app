99eb944667508e0d1089f24f013a2079d87c8293
Juan Diaz <jfdiaza@unal.edu.co>
2023-03-31 12:56:39 -0400

    create migrations files

---
+++ db/migrate/20230331165402_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :photo
+      t.text :bio
+      t.integer :postsCounter, default: 0
+
+      t.timestamps
+    end
+  end
+end
