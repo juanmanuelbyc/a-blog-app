1d9552353c03acf7dd8d7863e74d49cdf016f071
Juan Diaz <jfdiaza@unal.edu.co>
2023-04-07 15:34:20 -0400

    migrations changes

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_165559) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "comments_authors"
     t.index ["post_id"], name: "comments_posts"
-    t.index ["user_id"], name: "comments_authors"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "likes_authors"
     t.index ["post_id"], name: "likes_posts"
-    t.index ["user_id"], name: "likes_authors"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_165559) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "likes", "users"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "likes", "users", column: "post_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
