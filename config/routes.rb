fe535ef388c67e022247d624adb5ba9988f968a0
Juan Diaz <jfdiaza@unal.edu.co>
2023-04-13 15:13:24 -0400

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [ :new, :create]
+      resources :likes, only: [:create]
     end
   end
 end
