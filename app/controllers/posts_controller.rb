8df354fec6cdce5c6f6fbbdd920cad9d59d936fb
Juan Diaz <jfdiaza@unal.edu.co>
2023-04-13 01:31:11 -0400

    fix linters suggestions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
   end
+
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
