f4984f3200a37e0c75eb38a71c7d53dfa8b6b9fd
Botlhale Setou <botlhale.setou@gmail.com>
2023-04-22 20:25:12 +0200

    Fix routes.rb as per review

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create] do
-        resources :posts, only: [:index, :show, :create] do
-          resources :comments, only: [:index, :show, :create]
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
         end
       end
     end
