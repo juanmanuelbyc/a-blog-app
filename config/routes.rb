945aae0458f7a4be500dc8b5cf1de913cbc92724
Botlhale Setou <botlhale.setou@gmail.com>
2023-04-22 18:59:44 +0200

    Add application controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
+
+  namespace :api do
+    resources :users, only: [] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [ :new, :create, :destroy]
